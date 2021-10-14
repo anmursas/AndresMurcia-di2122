dbc0d3a96320e66ead5538a979318772cffd298b
anmursas <91335876+anmursas@users.noreply.github.com>
2021-10-14 22:49:29 +0200

    Delete 12.py

--- Unitat 1/activitats/12.py
+++
@@ -1,8 +0,0 @@
-import os
-os.getcwd()
-os.chdir('/home/anmursas')
-os.listdir('/home')
-os.mkdir('Nova_carpeta')
-os.rename('Nova_carpeta', 'Vella_carpeta')
-#os.remove('arxiu.txt')
-os.rmdir('Vella_carpeta')
